5cc482fbb0a1f9b6fbdc3ec69620b67e69eb5e98
user <athimbityana@gmail com>
2022-06-20 13:07:28 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int pasMark = 60;
+        int myMark = 59;
+
+        if (myMark>= pasMark){
+           System.out.println("You passed CP-SAT exam");
+
+        }
+        else
+        {
+            System.out.println("You failed the CP-SAT exam");
+        }
+    }
+}
